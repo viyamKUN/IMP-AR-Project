a8e5fcb4e6257c9e742e799d794094be14762e6c
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-03-23 19:42:45 +0900

    Add Creature Model
    * base model

---
+++ Assets/Scripts/Game Play/Models/Creatures.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+
+///<sumary>크리쳐 관련</summary>
+namespace Creatures
+{
+    public class Creature
+    {
+        public int ID;
+        public string Name;
+        public List<int> FavoriteItemIDs;
+        public int DroppedItemID;
+    }
+}
