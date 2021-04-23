08de0770bd16eee189b0c3fc82aa1e8e410842c3
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-04-23 17:33:45 +0900

    Add Requirement to Creature
    * It must have CreatureCareController

--- Assets/Scripts/Game Play/CreatureController.cs
+++ Assets/Scripts/Game Play/CreatureController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CreatureCareController))]
 public class CreatureController : MonoBehaviour
 {
     [SerializeField] private int _id;
