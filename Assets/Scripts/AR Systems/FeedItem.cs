7a7f3b8b460c3f6c8f45b8690a440d806c930471
SuyaFactory <77002255+suyaneeko@users.noreply.github.com>
2021-04-23 17:47:38 +0900

    Feed function script
    feed function script

---
+++ Assets/Scripts/AR Systems/FeedItem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class FeedItem : MonoBehaviour
+{
+    public GameObject FoodItem;
+    private GameObject food;
+    private ARRaycastManager raycastManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private GameObject spawned;
+
+
+    public void AddFoodItem()
+    {
+        food = Instantiate(FoodItem);
+    }
+
+    public void Update()
+    {
+        //if(TryGetTouchPosition(out Vector2 touchPosition))
+        if(Input.GetMouseButtonDown(0))
+        {
+            // create ray from the camera at the mouse position
+            //Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(raycastManager.Raycast(ray, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+                Instantiate(FoodItem, hitPose.position + new Vector3(0, 0.5f, 0), hitPose.rotation);
+                
+            }
+            
+        }        
+    }
+    
+}
